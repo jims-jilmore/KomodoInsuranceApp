0fdfd6af10e8bbe080950471812de21769be6394
James Gilmer <gilmer.jamesandrew@gmail.com>
2021-12-09 16:30:26 -0500

    First Commit

---
+++ KomodoApp.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Control Center. No Logic
+        }
+    }
+}
